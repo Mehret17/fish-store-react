e4a7b8ed817c1272c528aeeb322338cdc271b57d
Mehret Kulala <merutaye@gmail.com>
2018-07-09 20:46:03 -0500

    getting orders request by uid works

--- src/firebaseRequests/auth.js
+++ src/firebaseRequests/auth.js
@@ -11,4 +11,8 @@ const logoutUser = () => {
   return firebase.auth().signOut();
 };
 
-export default {registerUser, loginUser, logoutUser};
+const getUid = () => {
+  return firebase.auth().currentUser.uid;
+};
+
+export default {getUid, registerUser, loginUser, logoutUser};
