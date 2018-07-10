6931486f5fb042d3d93bd855d5a6e4c9f2773f3e
Mehret Kulala <merutaye@gmail.com>
2018-07-09 21:15:42 -0500

    displaying in new order works

--- src/components/New/New.js
+++ src/components/New/New.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import Fish from '../Fish/Fish';
+import Order from '../Order/Order';
+
+import fishRequests from '../../firebaseRequests/fishes';
 
 import './New.css';
 
 class New extends React.Component {
+  state = {
+    fishes: [],
+    order: {},
+  };
+
+  componentDidMount () {
+    fishRequests
+      .getRequest()
+      .then((fishes) => {
+        this.setState({fishes: fishes}); //({fishes}) object shorthand notation
+      })
+      .catch((err) => {
+        console.error('error with fish get request', err);
+      })
+  }
+
   render () {
+    const fishComponets = this.state.fishes.map((fish) => {
+      return (
+        <Fish 
+          key={fish.id}
+          details= {fish}
+        />
+      );
+    });
     return (
       <div className="New">
-      <h1>New</h1>
+      <div className="col-xs-8 inventory-container">
+      <h2>Inventory</h2>
+      <ul className="fishes">
+        {fishComponets}
+       </ul>
+       </div>
+       <Order />
       </div>
     );
   }
