aa125bf39015675cf4a153a8b598ee215dc8d81a
Mehret Kulala <merutaye@gmail.com>
2018-07-10 19:50:24 -0500

    saving order to firebase works

--- src/components/New/New.js
+++ src/components/New/New.js
@@ -3,6 +3,8 @@ import Fish from '../Fish/Fish';
 import Order from '../Order/Order';
 
 import fishRequests from '../../firebaseRequests/fishes';
+import authRequests from '../../firebaseRequests/auth';
+import orderRequests from '../../firebaseRequests/orders';
 
 import './New.css';
 
@@ -25,7 +27,18 @@ class New extends React.Component {
   }
 
   saveNewOrder = () => {
-
+    const newOrder  = {fishes: {...this.state.order}};
+    newOrder.uid = authRequests.getUid();
+    newOrder.dateTime = Date.now();
+    console.error('newOrder', newOrder);
+    orderRequests
+      .postRequest(newOrder)
+      .then(() => {
+        this.props.history.push('/orders');
+      })
+      .catch((err) => {
+        console.error('error in order post', err);
+      })
   }
 
   componentDidMount() {
